c6e84273389a6648268a6c3cb792f9d79b335148
Nishanth Kumar <nishanth.kumar20@gmail.com>
2024-02-22 18:45:34 -0500

    fix icon link

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,7 +26,7 @@ const config: GatsbyConfig = {
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
-                "icon": "src/images/ff_icon.png"
+                "icon": "src/images/ptp_icon.png"
             }
         }, "gatsby-plugin-mdx", {
             resolve: 'gatsby-source-filesystem',
